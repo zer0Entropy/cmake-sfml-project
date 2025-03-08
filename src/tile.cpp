872514c013f35dee61c5f0a1b493623c0254e3fc
Derek Muller <dsirus5@gmail.com>
2025-03-08 00:45:32 -0800

    Minor bugfix: InitTile function

--- src/tile.cpp
+++ src/tile.cpp
@@ -1,6 +1,6 @@
 #include "../include/tile.hpp"
 
-void InitTile(Tile& tile, std::string name, sf::Vector2u location, Terrain terrain) {
+void InitTile(Tile& tile, sf::Vector2u location, Terrain terrain) {
     tile.location = location;
     tile.terrain = terrain;
     tile.occupant = nullptr;
